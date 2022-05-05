30e52c3a466704d003081ed6d9f8113480be327d
attackoncs <2710651793@qq.com>
2022-05-05 12:42:48 +0800

    实现菜单显示

--- client.go
+++ client.go
@@ -11,13 +11,15 @@ type Client struct {
 	ServerPort int
 	Name string
 	conn net.Conn
+	flag int//当前client的模式
 }
 
 func NewClient(serverIp string,serverPort int) *Client {
 	//创建客户端对象
 	client:=&Client{
 		ServerIp: serverIp,
-		ServerPort: serverPort,
+		ServerPort: serverPort,	
+		flag:999,
 	}
 
 	//创建server
@@ -31,6 +33,43 @@ func NewClient(serverIp string,serverPort int) *Client {
 
 	//返回对象
 	return client
+}
+
+func (client *Client) menu() bool{
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+	if flag>=0&&flag<3{
+		client.flag=flag
+		return true
+	}else{
+		fmt.Println(">>>>请输入合法范围内的数字<<<<")
+		return false
+	}
+}
+
+func (client *Client) Run(){
+	for client.flag!=0{
+		for client.menu()!=true{
+		}
+
+		//根据不同模式处理不同业务
+		switch client.flag{
+		case 1:
+			fmt.Println("公聊模式...")
+			break
+		case 2:
+			fmt.Println("私聊模式...")
+			break
+		case 3:
+			fmt.Println("更新用户名选择...")
+			break
+		}
+	}
 }
 
 var serverIp string
@@ -52,6 +91,6 @@ func main() {
 	}
 	fmt.Println(">>>>>连接服务器成功...")
 
-	//启动客户端的业务，先阻塞
-	select{}
+	//启动客户端的业务
+	client.Run()
 }
