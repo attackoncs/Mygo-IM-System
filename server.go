5785f2d14deed8e03a603c2c28b73ecd120726fe
attackoncs <2710651793@qq.com>
2022-05-05 11:44:41 +0800

    完成超时强踢

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct{
@@ -57,8 +58,8 @@ func (this *Server) Handler(conn net.Conn){
 
 	user.Online()
 
-	//广播当前用户上线消息
-	this.Broadcast(user,"已上线")
+	//监听用户是否活跃的channel
+	isLive:=make(chan bool)
 
 	//接收客户端发送的数据
 	go func(){
@@ -79,11 +80,34 @@ func (this *Server) Handler(conn net.Conn){
 
 			//用户对msg的处理
 			user.DoMessage(msg)
+
+			//用户的任意消息，代表当前用户是活跃的
+			isLive<-true
 		}
 	}()
 
 	//当前handler阻塞
-	select{}
+	for{
+		select{
+		case<-isLive:
+			//当前用户是活跃的，应重置定时器
+			//不做任何事情，为激活select，更新下面定时器
+		case<-time.After(time.Second*10):
+			//已经超时
+			//将当前User强制关闭
+
+			user.SendMsg("你被踢了")
+
+			//销毁资源
+			close(user.C)
+
+			//关闭连接
+			conn.Close()
+
+			//推出当前Handler
+			return //runtime.Goexit()
+		}
+	}
 }
 
 //启动服务器的接口
